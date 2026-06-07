b87c29157cfff3e5ff46e8fdb71b7cd02a8e8de2
Andrew Usher <andrewusher00@gmail.com>
2026-06-07 18:51:01 -0500

    update version

---
+++ version-bump.mjs
@@ -0,0 +1,21 @@
+import { readFileSync, writeFileSync } from 'fs'
+
+const version = process.argv[2]
+if (!version) {
+  console.error('Usage: node version-bump.mjs <new-version>')
+  process.exit(1)
+}
+
+const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
+const pkg = JSON.parse(readFileSync('package.json', 'utf8'))
+const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
+
+manifest.version = version
+pkg.version = version
+versions[version] = manifest.minAppVersion
+
+writeFileSync('manifest.json', JSON.stringify(manifest, null, 2) + '\n')
+writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n')
+writeFileSync('versions.json', JSON.stringify(versions, null, 2) + '\n')
+
+console.log(`Bumped to v${version}`)
